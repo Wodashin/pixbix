017ad5db11222fe67e63801c0e8e85e38ef9e0a3
Wodashin <kevinaguilera97@gmail.com>
2025-06-02 23:30:04 -0400

    Update page.tsx

--- app/eventos/page.tsx
+++ app/eventos/page.tsx
@@ -1,9 +1,5 @@
 import { EventsPage } from "@/components/events-page"
 
 export default function EventosPage() {
-  return (
-    <div className="min-h-screen bg-slate-900">
-      <EventsPage />
-    </div>
-  )
+  return <EventsPage />
 }
