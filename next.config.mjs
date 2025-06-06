7c626400cc4361dd522e834100a56be5b42b1b4d
Wodashin <kevinaguilera97@gmail.com>
2025-06-05 23:48:05 -0400

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,33 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixbae-gaming.com',
+        port: '',
+        pathname: '/posts/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pub-e8d3b4b205fb43f594d31b93a69f816.r2.dev',
+        port: '',
+        pathname: '/posts/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
