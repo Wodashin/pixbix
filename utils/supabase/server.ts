81370c77cdb291cca76771718f802bf4bad2d6ff
Wodashin <kevinaguilera97@gmail.com>
2025-06-13 00:13:35 -0400

    Update server.ts

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,6 +1,8 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
+import type { Database } from "@/types/database.types"
 
 export const createClient = () => {
-  return createServerComponentClient({ cookies })
+  const cookieStore = cookies()
+  return createServerComponentClient<Database>({ cookies: () => cookieStore })
 }
