6842a9a865ccdecb2e29b91638ca410baf3ad5b4
Wodashin <kevinaguilera97@gmail.com>
2025-06-12 22:55:57 -0400

    Update client.ts

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,21 +1,3 @@
-import { createClient as supabaseCreateClient } from "@supabase/supabase-js"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 
-// Creamos un singleton para el cliente de Supabase en el navegador
-let supabaseClient: ReturnType<typeof supabaseCreateClient> | null = null
-
-export function createClient() {
-  if (supabaseClient) return supabaseClient
-
-  supabaseClient = supabaseCreateClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      auth: {
-        persistSession: true,
-        autoRefreshToken: true,
-      },
-    },
-  )
-
-  return supabaseClient
-}
+export const createClient = () => createClientComponentClient()
