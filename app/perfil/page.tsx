8f7753a0d9f95262c52b7db417292c55571b2d8f
Wodashin <kevinaguilera97@gmail.com>
2025-06-22 15:17:29 -0400

    Update page.tsx

--- app/perfil/page.tsx
+++ app/perfil/page.tsx
@@ -1,5 +1,5 @@
 import { Header } from "@/components/header"
-import { UserProfileRestored } from "@/components/user-profile-restored"
+import { UserProfileRestored } from "@/components/user-profilE"
 
 export default function PerfilPage() {
   return (
